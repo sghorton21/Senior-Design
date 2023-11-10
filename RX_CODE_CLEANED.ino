b1a2ffd09496630ec8dd68bafc1792587145b410
cortes-cc <149398200+cortes-cc@users.noreply.github.com>
2023-11-10 12:50:00 -0600

    Switched to U8G2 library 11-10-2023 12:50pm

--- RX_CODE_CLEANED.ino
+++ RX_CODE_CLEANED.ino
@@ -1,28 +1,30 @@
-// UPDATED 11-09-2023 3:15pm - CC
+// UPDATED 11-10-2023 12:50pm - CC
 
 #include <SPI.h>                  //SPI
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <Wire.h>
-#include <Adafruit_SSD1306.h>
+#include <U8g2lib.h>
 #include <Adafruit_DPS310.h>
 // #include <Fonts/FreeMono9pt7b.h>
 const byte address[6] = "00001";
-Adafruit_SSD1306 OLED_1(128, 64, &Wire, -1);
+const int DB_REGISTER = 0x0A;
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C OLED_1(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* reset=*/ 8);
 RF24 radio(9, 10); // CE, CSN (MAKE SURE THESE ARE THE RIGHT PINS ON ARDUINO)
 Adafruit_DPS310 dps;
-float SPL_DRONE, ALT_DRONE, ALT_GROUND, DELTA_ALT, SPL_PREDICT;
+TwoWire splRx;
+float SPL_DRONE, SPL_GROUND, ALT_DRONE, ALT_GROUND, DELTA_ALT, SPL_PREDICT;
 
 void setup() {
   Serial.begin(115200);
-  if(!OLED_1.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+  if(!OLED_1.begin()) { 
     Serial.println("SSD1306 allocation failed");
     for(;;);
   }
   delay(2000);
+  // OLED_1.setFont(&FreeMono9pt7b);
   OLED_1.clearDisplay();
-  OLED_1.setTextSize(1);             
-  OLED_1.setTextColor(WHITE);
+  OLED_1.setFont(u8g2_font_6x10_tf);
   dps.begin_I2C(0x77, &Wire);
   dps.configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
   dps.configureTemperature(DPS310_64HZ, DPS310_64SAMPLES);
@@ -34,16 +36,17 @@ void setup() {
 void loop() {
   GetString();
   GetAlt();
+  GetSPL(0x46);
+  UpdateOLED();
   DELTA_ALT = ALT_DRONE - ALT_GROUND;
   SPL_PREDICT = SPL_DRONE * 0.4; // placeholder equation
-  UpdateOLED();
-  delay(500);
+  // delay(500);
 }
 
 float GetAlt() {
   sensors_event_t temp_event, pressure_event;
   dps.getEvents(&temp_event, &pressure_event);
-  float ALT_GROUND = dps.readAltitude(1013.25);
+  ALT_GROUND = dps.readAltitude(1013.25);
   return ALT_GROUND;
 }
 
@@ -59,23 +62,32 @@ float GetString() {
   return SPL_DRONE, ALT_DRONE;
 }
 
+float GetSPL(int SPL_ADDRESS) {
+  splRx.beginTransmission(SPL_ADDRESS);
+  splRx.write(DB_REGISTER);
+  splRx.endTransmission(false);
+  splRx.requestFrom(SPL_ADDRESS, 1);
+  SPL_GROUND = splRx.read();
+  return SPL_GROUND;
+}
 void UpdateOLED() {
   OLED_1.clearDisplay();  // Clear the previous content
   OLED_1.setCursor(0, 0);      // Set "SPL Value: " at this position on OLED
-  OLED_1.print("Drone SPL: ");  // Print "SPL Value: " on OLED
-  OLED_1.print(SPL_DRONE);       // Print SPL receiving value on OLED
-  OLED_1.print(" dB");          // Print SPL units (dB) on OLED
-  OLED_1.setCursor(0,10);        // Set "Drone Alt: " at this position on OLED
-  OLED_1.print("Drone Altitude: ");   // Print "Drone Alt: " on OLED
-  OLED_1.print(ALT_DRONE);    // Print altitude value on OLED
-  OLED_1.print(" m");
-  OLED_1.setCursor(0, 20);      // Set "SPL Value: " at this position on OLED
-  OLED_1.print("Ground Altitude:: ");  // Print "SPL Value: " on OLED
+  // OLED_1.print("Drone SPL: ");  // Print "SPL Value: " on OLED
+  // OLED_1.print(SPL_DRONE);       // Print SPL receiving value on OLED
+  // OLED_1.print(" dB");          // Print SPL units (dB) on OLED
+  // OLED_1.setCursor(0,10);        // Set "Drone Alt: " at this position on OLED
+  // OLED_1.print("Drone Altitude: ");   // Print "Drone Alt: " on OLED
+  // OLED_1.print(ALT_DRONE);    // Print altitude value on OLED
+  // OLED_1.print(" m");
+  // OLED_1.setCursor(0, 20);      // Set "SPL Value: " at this position on OLED
+  OLED_1.print("Ground Altitude:: ");
+  OLED_1.setCursor(0, 10);  // Print "SPL Value: " on OLED
   OLED_1.print(ALT_GROUND);       // Print SPL receiving value on OLED
   OLED_1.print(" m");          // Print SPL units (dB) on OLED
   OLED_1.setCursor(0, 30);      // Set "SPL Value: " at this position on OLED
-  OLED_1.print("Predicted SPL: ");  // Print "SPL Value: " on OLED
-  OLED_1.print(SPL_PREDICT);       // Print SPL receiving value on OLED
+  OLED_1.print("Ground SPL: ");  // Print "SPL Value: " on OLED
+  OLED_1.print(SPL_GROUND);       // Print SPL receiving value on OLED
   OLED_1.print(" dB");          // Print SPL units (dB) on OLED
   OLED_1.display();  // Update the OLED display
 }
