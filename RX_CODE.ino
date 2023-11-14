00bf1d30e9d88d8ecd3a82b14bf3346dd767e9c6
cortes-cc <149398200+cortes-cc@users.noreply.github.com>
2023-11-14 14:00:30 -0600

    More variable conventions, cleanup

--- RX_CODE.ino
+++ RX_CODE.ino
@@ -1,4 +1,4 @@
-// UPDATED 11-13-2023 2:42pm - CC
+// UPDATED 11-14-2023 2:00pm - CC
 
 #include <SPI.h>
 #include <nRF24L01.h>
@@ -9,14 +9,14 @@
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C OLED_1(U8G2_R0, U8X8_PIN_NONE);
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C OLED_2(U8G2_R0, U8X8_PIN_NONE);
-RF24 NRF24L01(9, 10);
-Adafruit_DPS310 DPS310;
-TwoWire SPLMETER;
+RF24 nrf24l01(9, 10);
+Adafruit_DPS310 dps310;
+TwoWire i2cbus;
 
 const int BUTTON_1_PIN = 7;
 const int BUTTON_2_PIN = 8;
-uint8_t draw_state_1 = 0;
-uint8_t draw_state_2 = 0;
+uint8_t DRAW_STATE_1 = 0;
+uint8_t DRAW_STATE_2 = 0;
 float SPL_DRONE;
 float SPL_GROUND;
 float ALT_DRONE;
@@ -108,41 +108,41 @@ void OLED_2_page_2() {
 
 float GetAlt() {
   sensors_event_t temp_event, pressure_event;
-  DPS310.getEvents(&temp_event, &pressure_event);
-  ALT_GROUND = DPS310.readAltitude(1013.25);
+  dps310.getEvents(&temp_event, &pressure_event);
+  ALT_GROUND = dps310.readAltitude(1013.25);
   return ALT_GROUND;
 }
 
-float GetString() {
-  char combinedMessage[32]; 
-  NRF24L01.read(&combinedMessage, sizeof(combinedMessage));
-  String combinedStr = String(combinedMessage);
-  int separatorIndex = combinedStr.indexOf('|'); //Defines the index of | in the receiving string as an integer value
-  String splStr = combinedStr.substring(4, separatorIndex - 3); //Create a new string from the receiving string that contains the SPL value
-  String altitudeStr = combinedStr.substring(separatorIndex + 12, separatorIndex + 20); 
-  SPL_DRONE = splStr.toFloat();
-  ALT_DRONE = altitudeStr.toFloat();
+float ReceiveString() {
+  char RECEIVED_DATA[32]; 
+  nrf24l01.read(&RECEIVED_DATA, sizeof(RECEIVED_DATA));
+  String DATA_STRING = String(RECEIVED_DATA);
+  int SEPARATOR_INDEX = DATA_STRING.indexOf('|'); //Defines the index of | in the receiving string as an integer value
+  String SPL_DATA = DATA_STRING.substring(4, SEPARATOR_INDEX - 3); //Create a new string from the receiving string that contains the SPL value
+  String ALTITUDE_DATA = DATA_STRING.substring(SEPARATOR_INDEX + 12, SEPARATOR_INDEX + 20); 
+  SPL_DRONE = SPL_DATA.toFloat();
+  ALT_DRONE = ALTITUDE_DATA.toFloat();
   return SPL_DRONE, ALT_DRONE;
 }
 
 float GetSPL(int SPL_ADDRESS) {
-  SPLMETER.beginTransmission(SPL_ADDRESS);
-  SPLMETER.write(0x0A);
-  SPLMETER.endTransmission(false);
-  SPLMETER.requestFrom(SPL_ADDRESS, 1);
-  SPL_GROUND = SPLMETER.read();
+  i2cbus.beginTransmission(SPL_ADDRESS);
+  i2cbus.write(0x0A);
+  i2cbus.endTransmission(false);
+  i2cbus.requestFrom(SPL_ADDRESS, 1);
+  SPL_GROUND = i2cbus.read();
   return SPL_GROUND;
 }
 
 void UpdateOLED_1(void) {
-  switch(draw_state_1) {
+  switch(DRAW_STATE_1) {
     case 0: OLED_1_page_0(); break;
     case 1: OLED_1_page_1(); break;
     case 2: OLED_1_page_2(); break;
   }
 }
 void UpdateOLED_2(void) {
-  switch(draw_state_2) {
+  switch(DRAW_STATE_2) {
     case 0: OLED_2_page_0(); break;
     case 1: OLED_2_page_1(); break;
     case 2: OLED_2_page_2(); break;
@@ -155,12 +155,12 @@ void setup() {
   pinMode(BUTTON_2_PIN, INPUT_PULLUP);
   OLED_1.setI2CAddress(0x3C * 2);
   OLED_2.setI2CAddress(0x3D * 2);
-  // if (!NRF24L01.begin()) {
-  //   Serial.println("NRF24L01 connection failed!");
+  // if (!nrf24l01.begin()) {
+  //   Serial.println("nrf24l01 connection failed!");
   //   for(;;); 
   // }
-  // if (!DPS310.begin_I2C(0x77, &Wire)) {
-  //   Serial.println("DPS310 connection failed!");
+  // if (!dps310.begin_I2C(0x77, &Wire)) {
+  //   Serial.println("dps310 connection failed!");
   //   for(;;);
   // }
   // if(!OLED_1.begin()) { 
@@ -175,13 +175,13 @@ void setup() {
   OLED_1.setFont(u8g2_font_6x10_tf);  
   OLED_2.begin();
   OLED_2.setFont(u8g2_font_6x10_tf);
-  DPS310.begin_I2C(0x77, &Wire);
-  DPS310.configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
-  DPS310.configureTemperature(DPS310_64HZ, DPS310_64SAMPLES);
-  NRF24L01.begin();
-  NRF24L01.openReadingPipe(1, 00001);
-  NRF24L01.setPALevel(RF24_PA_MIN);
-  NRF24L01.startListening();
+  dps310.begin_I2C(0x77, &Wire);
+  dps310.configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
+  dps310.configureTemperature(DPS310_64HZ, DPS310_64SAMPLES);
+  nrf24l01.begin();
+  nrf24l01.openReadingPipe(1, 00001);
+  nrf24l01.setPALevel(RF24_PA_MIN);
+  nrf24l01.startListening();
 }
 
 void loop() {
@@ -191,7 +191,7 @@ void loop() {
   uint8_t BUTTON_2_LAST_STATE = 0;
   BUTTON_1_STATE = digitalRead(BUTTON_1_PIN);
   BUTTON_2_STATE = digitalRead(BUTTON_2_PIN);
-  GetString();
+  ReceiveString();
   GetAlt();
   GetSPL(0x46);
   DELTA_ALT = ALT_DRONE - ALT_GROUND;
@@ -203,18 +203,18 @@ void loop() {
   UpdateOLED_2();
   OLED_2.sendBuffer();
   if(BUTTON_1_STATE != BUTTON_1_LAST_STATE){
-      draw_state_1++;
-      // draw_state_2 = draw_state_1;
+      DRAW_STATE_1++;
+      // DRAW_STATE_2 = DRAW_STATE_1;
       delay(100);
-      if(draw_state_1 >= 3) {
-        draw_state_1 = 0;
+      if(DRAW_STATE_1 >= 3) {
+        DRAW_STATE_1 = 0;
       }
   }  
   if(BUTTON_2_STATE != BUTTON_2_LAST_STATE){
-      draw_state_2++;
+      DRAW_STATE_2++;
       delay(100);
-      if(draw_state_2 >= 3) {
-        draw_state_2 = 0;
+      if(DRAW_STATE_2 >= 3) {
+        DRAW_STATE_2 = 0;
       }
   }
 }
